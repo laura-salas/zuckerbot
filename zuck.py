b11b77567a7491338ea83310d9b678ad6b0ec035
Laura Salas <laurasalas@Lauras-MacBook-Air.local>
2021-11-03 15:19:37 -0400

    restructured a little to make bot more generic

--- zuck.py
+++ zuck.py
@@ -13,25 +13,27 @@ MOD_CHANNEL_ID = int(os.getenv('MOD_CHANNEL_ID'))
 EMOJI_CHECKMARK = '✅'
 EMOJI_ENVELOPE = '✉️'
 
-# discord shtuff
+# discord stuff
 client = discord.Client()
 intents = discord.Intents.default()
 intents.reactions = True
 intents.members = True
-bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+bot = commands.Bot(command_prefix='$', description=description,  intents=intents)
 
 ################################
 # MESSAGES #
 
 # Reactions to verify user
-verified_message = "Your account has been verified and your access has been granted! Feel free to check " \
+BOT_NAME = "zuck"
+
+VERIFIED_MESSAGE = "Your account has been verified and your access has been granted! Feel free to check " \
                    "any of the verified channels now :-) "
-further_verification = "The mod team would like to ask a couple questions about your verification proof. " \
+FURTHER_VERIFICATION = "The mod team would like to ask a couple questions about your verification proof. " \
                        "\nDon't worry! A mod will be with you soon.\n\nRemember, you can also dm the mods " \
-                       "with `$zuck dm_mods` + the message you want to send! "
+                       "with `$%s dm_mods` + the message you want to send! " % BOT_NAME
 
 # DMing Bot
-initial_greeting = """Hello! This is the verifier bot for the FB Interns discord.   
+INITIAL_GREETING = """Hello! This is the verifier bot for the FB Interns discord.   
 
 To make things easier for the mods and to accelerate the verification process, **please reply here with a screenshot 
 of your __new hire checklist__ and your full name in the right top corner (so you may need to click on the dropdown box). 
@@ -41,34 +43,36 @@ Important: __You must have already accepted your offer for your verified status 
 After sending this, a moderator will review this and I will let you know when your access has been approved, 
 or if further verification is required. 
 
-If you need further assistance, please type `$zuck dm_mods` so I can send someone your way. You may choose to send a 
-custom message to the mods by typing said message right after `$zuck dm_mods`.
+If you need further assistance, please type `$%s dm_mods` so I can send someone your way. You may choose to send a 
+custom message to the mods by typing said message right after `$%s dm_mods`. 
 
-The mod team wishes you the best and hopes you have a successful 2022 internship!"""
+The mod team wishes you the best and hopes you have a successful 2022 internship!""" % (BOT_NAME, BOT_NAME)
 
-help_is_on_way = "Help is on the way! Please wait while one of our moderators contacts you. "
+HELP_IS_ON_WAY = "Help is on the way! Please wait while one of our moderators contacts you. "
 
-proof_received = "Proof received. Mods will review it shortly :)"
+PROOF_RECEIVED = "Proof received. Mods will review it shortly :)"
 
-command_help = "Commands:  `$zuck verify` to get verification instructions. `$zuck dm_mods` to speak to a mod. You " \
+COMMAND_HELP = "Commands:  `$%s verify` to get verification instructions. `$%s dm_mods` to speak to a mod. You " \
                "may choose to send a custom message to the mods by typing said message right after command " \
-               "`$zuck dm_mods`. "
+               "`$%s dm_mods`. " % (BOT_NAME, BOT_NAME, BOT_NAME)
 
-invalid_command = "Beep, boop, I did not get that. "
+INVALID_COMMAND = "Beep, boop, I did not get that. "
 
 # Notifying mods about user
-notification_verify = "<@%s> has asked for the above image to be used as proof of verification. React with %s to " \
+NOTIFICATION_VERIFY = "<@%s> has asked for the above image to be used as proof of verification. React with %s to " \
                       "approve the user, or %s to let them know you will be contacting them for further info. "
 
-notification_help = "User <@%s> has asked for a mod to contact them for further discussion.  "
+NOTIFICATION_HELP = "User <@%s> has asked for a mod to contact them for further discussion.  "
 
-
+BOT_JOKE_PHRASE = "I asked my developers to make a better version of JS. I knew this was a huge " \
+                                   "thing to ask - but looking back, they did React pretty well!"
 ################################
 
 @client.event
 async def on_reaction_add(reaction, _user):
     """
     Help mods attribute "verified" role by simply clicking on a react button
+    :param _user: [unused param]
     :param reaction: a reaction object to the message. Includes
         - reaction.count , the amount of people having reacted with this emoji
         - reaction.message , the message object within the reaction (see param of on_message for more info)
@@ -79,15 +83,18 @@ async def on_reaction_add(reaction, _user):
         if str(reaction.emoji) == EMOJI_CHECKMARK:
             role = discord.utils.get(reaction.message.guild.roles, name="verified")
             await userino.add_roles(role)
-            await userino.send(verified_message)
+            await userino.send(VERIFIED_MESSAGE)
         elif str(reaction.emoji) == EMOJI_ENVELOPE:
-            await userino.send(further_verification)
+            await userino.send(FURTHER_VERIFICATION)
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
     """
-    The core exchange between zuckerbot and a user
+    The core exchange between bot and a user
     :param message: the message object sent to the bot. Includes
         - message.channel
         - message.content (the string of a message)
@@ -98,36 +105,37 @@ async def on_message(message):
 
     if message.author == client.user:
         return
-    if message.content.startswith("$zuck tell me a joke"):  # little easter egg
-        await message.channel.send("I asked my developers to make a better version of JS. I knew this was a huge "
-                                   "thing to ask - but looking back, they did React pretty well!")
+    if message.content.startswith("$%s tell me a joke" % BOT_NAME):  # little easter egg
+        await message.channel.send(BOT_JOKE_PHRASE)
 
     elif isinstance(message.channel, discord.channel.DMChannel):
         # send proof to mod channel, notify mods, and offer reaction options
         if len(message.attachments) != 0:
-            await message.channel.send(proof_received)
+            await message.channel.send(PROOF_RECEIVED)
             await mod_channel.send(message.attachments[0])
-            last_message = await mod_channel.send(notification_verify % (message.author.id,
+            last_message = await mod_channel.send(NOTIFICATION_VERIFY % (message.author.id,
                                                                          EMOJI_CHECKMARK,
                                                                          EMOJI_ENVELOPE))
             await last_message.add_reaction(EMOJI_CHECKMARK)
             await last_message.add_reaction(EMOJI_ENVELOPE)
 
         # display info for user wanting to verify themselves or to message mods.
-        elif str(message.content).lower().startswith("$zuck verify"):
-            await message.channel.send(initial_greeting)
-        elif str(message.content).lower().startswith("$zuck dm_mods"):
-            await message.channel.send(help_is_on_way)
-            await mod_channel.send(notification_help % message.author.id)
-            if len(message.content) > len("$zuck dm_mods") + 1:  # if user has attached an additional message
-                await mod_channel.send("They also sent the following message: \"%s\"" % message.content[
-                                                                                        len("$zuck dm_mods") + 1:])
-        elif str(message.content).lower().startswith("$zuck") or not str(message.content).lower().startswith("$zuck"):
-            if not str(message.content).lower().startswith("$zuck") or len(message.content) > len("$zuck") + 1:
-                await message.channel.send(invalid_command)
-            await message.channel.send(command_help)
+        elif str(message.content).lower().startswith("$%s verify" % BOT_NAME):
+            await message.channel.send(INITIAL_GREETING)
+        elif str(message.content).lower().startswith("$%s dm_mods" % BOT_NAME):
+            await message.channel.send(HELP_IS_ON_WAY)
+            await mod_channel.send(NOTIFICATION_HELP % message.author.id)
+            if len(message.content) > len("$%s dm_mods") + 1:  # if user has attached an additional message
+                await mod_channel.send("They also sent the following message: \"%s\""
+                                       % message.content[len("$%s dm_mods" % BOT_NAME) + 1:])
+        elif str(message.content).lower().startswith("$%s" % BOT_NAME) or \
+                not str(message.content).lower().startswith("$%s" % BOT_NAME):
+            if not str(message.content).lower().startswith("$%s" % BOT_NAME) or len(message.content) >\
+                    len("$%s" % BOT_NAME) + 1:
+                await message.channel.send(INVALID_COMMAND)
+            await message.channel.send(COMMAND_HELP)
 
-    elif message.content.startswith("$zuck"):  # if users try to call it outside of DMs.
+    elif message.content.startswith("$%s" % BOT_NAME):  # if users try to call it outside of DMs.
         await message.channel.send("I do not [yet] respond to normal commands outside of DMs :(")
 
 client.run(TOKEN)
